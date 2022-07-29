885a37051c9defb14d06aaaf751b56ccae9911d7
Eriatj <100821142+Eriatj@users.noreply.github.com>
2022-07-29 04:38:04 -0300

    Código inicial da calculadora

---
+++ src/calculadora.rb
@@ -0,0 +1,22 @@
+class Calculadora
+
+    def soma(a, b)
+        a + b
+    end
+
+    def subtracao(a, b)
+        (a) - (b)
+    end
+
+    def multiplicacao(a, b)
+        a * b
+    end
+
+    def divisao(a, b)
+        a / b 
+    end
+
+    def potencia(a, b)
+        a ** b
+    end
+end
